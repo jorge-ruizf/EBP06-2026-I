19958d09bcd6649b5a1f30461f88a8094c5f3ac0
miguel-cqr <m.cerquera@udea.edu.co>
2026-04-02 11:56:31 -0500

    configuración inicial de Docker y base de datos

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -1,27 +1,63 @@
 -- Flyway baseline migration (tables simplified)
 CREATE TABLE IF NOT EXISTS users (
   id BIGINT PRIMARY KEY,
-  username VARCHAR(100) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  full_name VARCHAR(100) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  state VARCHAR(20) NOT NULL,
+  register_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS category (
   id BIGINT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  type VARCHAR(20) NOT NULL,
+  description VARCHAR(500),
+  state VARCHAR(20) NOT NULL,
+  user_id BIGINT ,
+  CONSTRAINT fk_user_category FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS transaction (
   id BIGINT PRIMARY KEY,
+  date TIMESTAMP NOT NULL,
   amount DECIMAL(19,2) NOT NULL,
   description VARCHAR(500),
-  category_id BIGINT,
+  type VARCHAR(20) NOT NULL,
+  state VARCHAR(20) NOT NULL,
   user_id BIGINT,
-  CONSTRAINT fk_transaction_category FOREIGN KEY (category_id) REFERENCES category(id),
-  CONSTRAINT fk_transaction_user FOREIGN KEY (user_id) REFERENCES users(id)
+  CONSTRAINT fk_user_transaction FOREIGN KEY (user_id) REFERENCES users(id),
+  category_id BIGINT,
+  CONSTRAINT fk_category_transaction FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
 CREATE TABLE IF NOT EXISTS budget (
   id BIGINT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  limit_amount DECIMAL(19,2)
+  month INT NOT NULL,
+  year INT NOT NULL,
+  limit_amount DECIMAL(19,2),
+  user_id BIGINT,
+  CONSTRAINT fk_user_budget FOREIGN KEY (user_id) REFERENCES users(id),
+  category_id BIGINT,
+  CONSTRAINT fk_category_budget FOREIGN KEY (category_id) REFERENCES category(id)
 );
+
+CREATE TABLE IF NOT EXISTS recovery_token (
+  id BIGINT PRIMARY KEY,
+  token VARCHAR(255) NOT NULL,
+  generated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  used BOOLEAN NOT NULL DEFAULT FALSE,
+  expiry_date TIMESTAMP NOT NULL,
+  user_id BIGINT,
+  CONSTRAINT fk_user_recovery_token FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS session(
+  id BIGINT PRIMARY KEY,
+  token VARCHAR(255) NOT NULL,
+  generated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  expiry_date TIMESTAMP NOT NULL,
+  state VARCHAR(20) NOT NULL,
+  user_id BIGINT,
+  CONSTRAINT fk_user_session FOREIGN KEY (user_id) REFERENCES users(id)
+)
